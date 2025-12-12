0e8b9485731c8712b12b31a6197de562534879fe
xiaomeng9597 <519561910@qq.com>
2025-12-12 10:17:03 +0800

    update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -189,6 +189,7 @@ cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3588-orangepi-5-plus.dts target/linux/
 # 集成 nsy_g68-plus WiFi驱动
 mkdir -p package/base-files/files/lib/firmware/mediatek
 cp -f $GITHUB_WORKSPACE/configfiles/WirelessDriver/mt7916_eeprom.bin package/base-files/files/lib/firmware/mediatek/mt7916_eeprom.bin
+cp -f $GITHUB_WORKSPACE/configfiles/WirelessDriver/mt7916_eeprom_backup.bin package/base-files/files/lib/firmware/mediatek/mt7916_eeprom_backup.bin
 
 
 # rtl8367b驱动资源包，暂时使用这样替换
